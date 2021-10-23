b3e28aa90b1cf41f6568da02ebef9abeb1b8d5f2
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-23 13:18:25 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+// const newNum = numbers.map(num => {
+//   return num * 2;
+// });
+// console.log(newNum);
+
+//Filter - Create a new array by keeping the items that return true.
+// const newNum = numbers.filter(num => {
+//   return num > 10;
+// });
+// console.log(newNum);
+
+// let newNum = [];
+// numbers.forEach(num => {
+//   if (num < 10) newNum.push(num);
+// });
+// console.log(newNum);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// const newNum = numbers.reduce((accumulator, num) => {
+//   return accumulator + num;
+// });
+// console.log(newNum);
+
+// let newNum = 0;
+// numbers.forEach(num => {
+//   newNum += num;
+// });
+// console.log(newNum);
+
+//Find - find the first item that matches from an array.
+// const newNum = numbers.find(num => {
+//   return num > 10;
+// });
+// console.log(newNum);
+
+//FindIndex - find the index of the first item that matches.
+// const newNum = numbers.findIndex(num => {
+//   return num > 10;
+// });
+// console.log(newNum);
